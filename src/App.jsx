37155c2a60ff1881e8ecc6619f7fa36c7df1742a
abimiku oshela <abimikuoshela@gmail.com>
2025-06-19 22:06:46 +0100

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Nav/>
+     <Hero/>
     </>
   )
 }
