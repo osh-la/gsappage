90870d90214b0b606774206da5a9a1d117c54c09
abimiku oshela <abimikuoshela@gmail.com>
2025-07-15 12:07:59 +0100

    s

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,13 +5,13 @@ const Nav = () => {
     <nav className="fixed top-0 w-full z-50 bg-red-50 shadow-sm text-gray-800 text-base sm:text-xl px-4 sm:px-8 py-4">
       <div className="flex items-center justify-between">
         
-        {/* Left */}
+        
         <div className="font-bold">LOGO</div>
 
-        {/* Center */}
+       
         <div className="hidden sm:block">MENU</div>
 
-        {/* Right */}
+  
         <div className="flex gap-4">
           <span className="cursor-pointer hover:underline">SHOP</span>
           <span className="cursor-pointer hover:underline">CART</span>
